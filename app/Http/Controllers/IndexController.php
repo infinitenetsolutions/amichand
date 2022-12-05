4868dfd10916e59d53cba0e64676069388c432d3
raja <raja@raja.com>
2022-12-05 16:41:40 +0530

    all done

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,9 +11,8 @@ class IndexController extends Controller
 
     function index()
     {
-   
-        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id` group by `products`.`id` limit 9');
-        $services = DB::select('select `services`.`id` AS `id`,`services`.`category` AS `category`,`services`.`log_title` AS `log_title`,`services`.`slug` AS `slug`,`services`.`log_description` AS `log_description`,`service_images`.`name` AS `img_name` from (`services` join `service_images`) where `services`.`id` = `service_images`.`service_id` group by `services`.`id` limit 9');
+        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id` group by `products`.`id` order by `products`.id desc   limit 9');
+        $services = DB::select('select `services`.`id` AS `id`,`services`.`category` AS `category`,`services`.`log_title` AS `log_title`,`services`.`slug` AS `slug`,`services`.`log_description` AS `log_description`,`service_images`.`name` AS `img_name` from (`services` join `service_images`) where `services`.`id` = `service_images`.`service_id`  group by `services`.`id`  order by `services`.id desc limit 9');
         return response()->json(['products' => $products, 'services' => $services]);
     }
 
