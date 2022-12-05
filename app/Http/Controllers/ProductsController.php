fceb5e85a4af22c3881917fcb8b0cc1eb2ffb5fb
raja <raja@raja.com>
2022-12-05 16:33:41 +0530

    all done

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,10 +10,7 @@ class ProductsController extends Controller
     function index($category)
     {
         $cat=DB::table('product_categories')->where('name','like','%'.$category.'%')->first();
-     
-
         $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id` group by `products`.`id` && `category`='.$cat->id.'');
-
        $category = DB::table('product_categories')->get();
        return response()->json(['products' => $products, 'category' => $category]);
     }
