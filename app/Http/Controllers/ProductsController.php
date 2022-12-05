16aa311cda48ff66da4eba57d36eef9ad6de8218
raja <raja@raja.com>
2022-12-05 16:52:38 +0530

    all done

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,7 +10,7 @@ class ProductsController extends Controller
     function index($category)
     {
         $cat=DB::table('product_categories')->where('name','like','%'.$category.'%')->first();
-        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id`  && `category`='.$cat->id.' group by `products`.`id`');
+        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id`  && `category`='.$cat->id.' group by `products`.`id` order by `products`.`id` desc');
        $category = DB::table('product_categories')->get();
        return response()->json(['products' => $products, 'category' => $category]);
     }
@@ -19,7 +19,7 @@ class ProductsController extends Controller
         $product =  DB::table('products')->where('slug', $slug)->first();
         $feature = DB::table('product_features')->where('product_id', $product->id)->get();
         $images = DB::table('product_images')->where('product_id', $product->id)->get();
-        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id` group by `products`.`id` LIMIT 20');
+        $products = DB::select('select `products`.`id` AS `id`,`products`.`category` AS `category`,`products`.`log_title` AS `log_title`,`products`.`slug` AS `slug`,`products`.`log_description` AS `log_description`,`product_images`.`name` AS `img_name` from (`products` join `product_images`) where `products`.`id` = `product_images`.`product_id` group by `products`.`id` LIMIT 20 order by `products`.`id` desc');
         $category = DB::table('product_categories')->get();
         return response()->json(['product' => $product, 'feature' => $feature, 'images' => $images, 'products' => $products, 'category' => $category]);
     }
