69459f96191f528a0cd64c70f5daf4dd8db8e2a8
raja <raja@raja.com>
2022-12-26 16:38:02 +0530

    all done

---
+++ resources/js/components/blog.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <!-- Blog Section Start -->
+        <section class="blog-section bg-f8">
+            <div class="container">
+                <div class="row">
+                    <div class="section-title">
+                        <h4>OUR blog</h4>
+                        <h2>Latest <span>blog</span></h2>
+                        <p>Consectetur adipisicing elit. Excepturi nemo tenetur veniam repellat earum inventore modi
+                            omnis.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-lg-4">
+                        <div class="blog-post">
+                            <div class="thumb">
+                                <img alt="" src="/images/blog/1.jpg">
+                            </div>
+                            <div class="date-box">
+                                <div class="inner">
+                                    <div class="date">
+                                        <b>24</b> sep
+                                    </div>
+                                    <div class="comment">
+                                        <i class="fa fa-commenting-o" aria-hidden="true"></i> 8
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content">
+                                <h3>HERE WILL BE THE TITLE</h3>
+
+                                <p>There are many variations passages available, but the lorem, ipsum... </p>
+                            </div>
+                            <a href="blog-details.html" class="read-btn">Continue Reading
+                                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="blog-post">
+                            <div class="thumb">
+                                <img alt="" src="/images/blog/2.jpg">
+                            </div>
+                            <div class="date-box">
+                                <div class="inner">
+                                    <div class="date">
+                                        <b>26</b> sep
+                                    </div>
+                                    <div class="comment">
+                                        <i class="fa fa-commenting-o" aria-hidden="true"></i> 2
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content">
+                                <h3>HERE WILL BE THE TITLE</h3>
+
+                                <p>There are many variations passages available, but the lorem, ipsum... </p>
+                            </div>
+                            <a href="blog-details.html" class="read-btn">Continue Reading
+                                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <div class="blog-post">
+                            <div class="thumb">
+                                <img alt="" src="/images/blog/3.jpg">
+                            </div>
+                            <div class="date-box">
+                                <div class="inner">
+                                    <div class="date">
+                                        <b>30</b> sep
+                                    </div>
+                                    <div class="comment">
+                                        <i class="fa fa-commenting-o" aria-hidden="true"></i> 9
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content">
+                                <h3>HERE WILL BE THE TITLE</h3>
+
+                                <p>There are many variations passages available, but the lorem, ipsum... </p>
+                            </div>
+                            <a href="blog-details.html" class="read-btn">Continue Reading
+                                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Blog Section End -->
+    </div>
+</template>
